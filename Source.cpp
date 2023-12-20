358e9cfb917588fea1c27216944a06a7fc78d48a
seif <seif@gmail.com>
2023-12-20 21:35:29 +0200

    add sum function

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int sum(int a,int b);
+
 void main()
 {
 	cout << "Hello world" << endl;
 cout << "welcome homies" << endl;
+cout<<sum(2,4);
+}
+
+int sum (int a,int b)
+{
+return a+b;
 }
